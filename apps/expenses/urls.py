06f5030f0a62176a3a3fd5af7ca45ee7060c97be
landry-dev <ralphsengue@gmail.com>
2026-02-25 12:21:27 +0100

    fix: remove broken ExpenseExportExcelView import and use DefaultRouter for export_excel action

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.expenses.views import ExpenseViewSet, ExpenseCategoryViewSet, ExpenseExportExcelView
+from apps.expenses.views import ExpenseViewSet, ExpenseCategoryViewSet
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewSet, basename='expense')
 router.register(r'categories', ExpenseCategoryViewSet, basename='category')
 
-# Vue directe pour l'export Excel
 urlpatterns = [
-    path('expenses/export_excel/', ExpenseExportExcelView.as_view(), name='export-excel'),
     path('', include(router.urls)),
 ]
