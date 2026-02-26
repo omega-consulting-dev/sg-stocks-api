c8a390096f8361f5965e4d730983bf2ed03dcc2d
landry-dev <ralphsengue@gmail.com>
2026-02-26 11:37:35 +0100

    "Fix: correction URL export Excel"

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from apps.expenses.views import ExpenseViewSet, ExpenseCategoryViewSet
 
 router = DefaultRouter()
-router.register(r'expenses', ExpenseViewSet, basename='expense')
+router.register(r'', ExpenseViewSet, basename='expense')
 router.register(r'categories', ExpenseCategoryViewSet, basename='category')
 
 urlpatterns = [
